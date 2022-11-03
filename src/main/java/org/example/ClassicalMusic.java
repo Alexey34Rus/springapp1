c025188baa3269a91ca53037d93ba8e9aa1453d8
alexe <prograbota@mail.ru>
2022-11-03 12:53:52 +0300

    init destroy factory - method

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,16 @@
 package org.example;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic () {} //чтобы нельзя было создать объект через new
+    public static ClassicalMusic getClassicalMusic () {
+        return new ClassicalMusic();
+    }
+    public void doMyInit () {
+        System.out.println("Doing my initializaition");
+    }
+    public void doMyDestroy () {
+        System.out.println("Doing my destraction");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
